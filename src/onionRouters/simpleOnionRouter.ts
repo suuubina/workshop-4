092c550fb293d04ec5922f13a752c4bb530b87f5
suuubina <153621023+suuubina@users.noreply.github.com>
2024-02-27 08:55:28 +0100

    Update simpleOnionRouter.ts

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -10,6 +10,28 @@ export async function simpleOnionRouter(nodeId: number) {
   // TODO implement the status route
   // onionRouter.get("/status", (req, res) => {});
 
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: string | null = null;
+
+
+  onionRouter.get('/status', (req, res) => {
+    res.send('live')
+  })
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
+
+
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
       `Onion router ${nodeId} is listening on port ${
