ff83472913820a6917131083d61cab287965ca7e
suuubina <153621023+suuubina@users.noreply.github.com>
2024-02-27 09:07:46 +0100

    Update simpleOnionRouter.ts

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -10,14 +10,32 @@ export async function simpleOnionRouter(nodeId: number) {
   // TODO implement the status route
   // onionRouter.get("/status", (req, res) => {});
 
+
   let lastReceivedEncryptedMessage: string | null = null;
   let lastReceivedDecryptedMessage: string | null = null;
   let lastMessageDestination: string | null = null;
 
 
-  app.get('/status/', (req, res) => {
-    res.send('live');
+  // Status Route : 
+  onionRouter.get('/status', (req, res) => {
+    res.send('live')
+  })
+
+  // Route to get the last received encrypted message
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
   });
+
+  // Route to get the last received decrypted message
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  // Route to get the last message destination
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
+
 
 
 
