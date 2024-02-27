acc7182b49a7f2eb6d8b6b16d0eb490818f52db8
suuubina <153621023+suuubina@users.noreply.github.com>
2024-02-27 09:22:13 +0100

    Update user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -7,16 +7,17 @@ export type SendMessageBody = {
   destinationUserId: number;
 };
 
+let lastReceivedMessage: string | null = null;
+let lastSentMessage: string | null = null;
+
 export async function user(userId: number) {
   const _user = express();
   _user.use(express.json());
   _user.use(bodyParser.json());
 
   // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
-
-    _user.get("/status", (req, res) => {
-    res.send('live')
+   _user.get("/status", (req, res) => {
+      res.send('live')
 
    });
 
@@ -27,7 +28,6 @@ export async function user(userId: number) {
    _user.get("/getLastSentMessage", (req, res) => {
     res.json({ result: lastSentMessage})
    });
-
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
