790bada7f55f816f5811084f3e9383687bdaf965
suuubina <153621023+suuubina@users.noreply.github.com>
2024-03-11 12:39:37 +0100

    Update user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,7 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_USER_PORT, NODE_REGISTRY } from "../config";
-import { symEncrypt, createRandomSymmetricKey, exportSymKey, exportPubKey } from "./crypto";
+import { BASE_USER_PORT } from "../config";
+import { symEncrypt, createRandomSymmetricKey } from "./crypto";
 
 export type SendMessageBody = {
   message: string;
@@ -19,35 +19,10 @@ async function user(userId: number) {
 
   _user.post("/sendMessage", async (req, res) => {
     const { message, destinationUserId }: SendMessageBody = req.body;
-    const publicKey = NODE_REGISTRY[destinationUserId].publicKey;
-    const circuit = Array.from({ length: 3 }, () => {
-      return NODE_REGISTRY[Math.floor(Math.random() * NODE_REGISTRY.length)];
-    });
-    let encryptedMessage = await symEncrypt(await createRandomSymmetricKey(), message);
-    let destination = destinationUserId.toString().padStart(10, "0");
-    for (const node of circuit) {
-      const symmetricKey = await createRandomSymmetricKey();
-      const encryptedSymmetricKey = await symEncrypt(publicKey, symmetricKey);
-      const previousValue = destination + encryptedMessage;
-      encryptedMessage = await symEncrypt(symmetricKey, previousValue + encryptedSymmetricKey);
-      destination = node.id.toString().padStart(10, "0");
-    }
-    const entryNode = circuit[0];
-    const entryNodeUrl = `http://localhost:${entryNode.port}/message`;
-    fetch(entryNodeUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ message: encryptedMessage }),
-    })
-      .then(() => {
-        res.json({ message: "Message sent successfully" });
-      })
-      .catch((error) => {
-        console.error("Error sending message:", error);
-        res.status(500).json({ error: "Internal server error" });
-      });
+    const destinationPort = Math.floor(Math.random() * 1000) + 4000; 
+    const encryptedMessage = await symEncrypt(await createRandomSymmetricKey(), message);
+    console.log(`Sending encrypted message to port ${destinationPort}: ${encryptedMessage}`);
+    res.json({ message: "Message sent successfully" });
   });
 
   _user.post("/message", async (req, res) => {
@@ -64,4 +39,3 @@ async function user(userId: number) {
 }
 
 export { user };
-
